da1a5d63ef227499d0b80585469ab1a4eb879d30
zhe.sun <zhe.sun@horizon.ai>
2020-03-21 13:52:00 +0800

    add Bit:power-of-four

---
+++ Bit/power-of-four.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    bool isPowerOfFour(int num) {
+        // 是否是2的幂
+        if (num & num-1 != 0) return false;
+        if ((num & 0x55555555) == num) return true;   // 注意==优先级高于&
+        else return false;
+    }
+};
